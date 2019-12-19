680180dfbb840b5e1ad130a0dc679ff8f2fc4b25
James <jinglis12@googlemail.com>
2019-12-19 11:46:04 +0000

    Initial commit

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
+require_relative '../bin/environment.rb'
+
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+    sql = <<-SQL
+    insert into pokemon (name, type) values(?, ?)
+    SQL
+
+    DB[:conn].execute(sql, name, type)
+    end
 end
